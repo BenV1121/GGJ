e237d4d8cfde76c9616834ca255ad16e3bc4fb88
unknown <christopher.j.fanning@gmail.com>
2018-01-27 14:49:41 -0800

    Add enemymanager stuff

---
+++ Assets/Scripts/Enemy/RetroAttack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace CompleteProject
+{
+    public class RetroAttack : EnemyAttack
+    {
+        public Transform bullet;
+
+        override protected void Attack ()
+        {
+            // Reset the timer.
+            timer = 0f;
+
+            // If the player has health to lose...
+            if(playerHealth.currentHealth > 0)
+            {
+                var angle = this.GetComponent<Transform>().rotation;
+                var shot = Instantiate(bullet, GetComponent<Transform>().position, angle);
+                shot.GetComponent<Rigidbody>().velocity = angle * new Vector3(0, 0, 30);
+            }
+        }
+    }
+}
