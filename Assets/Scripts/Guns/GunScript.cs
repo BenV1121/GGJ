63b01e85be64481952ba76e1712e0c9499c30cad
immefrank <frankzhang93@gmail.com>
2018-01-27 16:16:05 -0800

    Add gun scripts
    rt.

---
+++ Assets/Scripts/Guns/GunScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CompleteProject
+{
+    public abstract class GunScript : MonoBehaviour
+    {
+
+        //Shoot
+        public abstract void Shoot(float timer,PlayerShooting shootingControl);
+
+        //Effect end
+        public abstract void DisableEffects();
+
+        //Switch (needs to be called before shoot)
+        public abstract void SwitchIn();
+
+        public abstract void SwitchOut();
+    }
+}
