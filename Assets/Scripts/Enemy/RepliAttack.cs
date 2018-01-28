5083aa5f32f956876cf377f4f7a059af6af307c6
unknown <christopher.j.fanning@gmail.com>
2018-01-28 15:07:39 -0800

    Fix enemy bullets

--- Assets/Scripts/Enemy/RepliAttack.cs
+++ Assets/Scripts/Enemy/RepliAttack.cs
@@ -12,10 +12,20 @@ public class RepliAttack : EnemyAttack
         // If the player has health to lose...
         if (playerHealth.lives > 0)
         {
-            var angle = this.transform.rotation;
-            Vector3 dir = (this.transform.position - player.transform.position).normalized;
-            var shot = Instantiate(bullet, GetComponent<Transform>().position, angle);
-            shot.GetComponent<Rigidbody>().velocity = angle * new Vector3(0, 0, 30);
+
+            Vector3 dir = player.transform.position - transform.position;
+            dir = player.transform.InverseTransformDirection(dir);
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            var rot = Quaternion.AngleAxis(angle, Vector3.up);
+
+            /*
+var shot = Instantiate(bullet, GetComponent<Transform>().position, Vector3.forward);
+shot.rotation = rot;
+shot.GetComponent<Rigidbody>().velocity = angle * new Vector3(0, 0, 30);
+
+*/
+            var shot = Instantiate(bullet, new Vector3(transform.position.x, 0.5f, transform.position.z-3), new Quaternion());
+            shot.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -30);
         }
     }
 }
--- Assets/Scripts/Enemy/RepliMovement.cs
+++ Assets/Scripts/Enemy/RepliMovement.cs
@@ -18,8 +18,8 @@ public class RepliMovement : MonoBehaviour
         playerHealth = player.GetComponent<PlayerHealth>();
         enemyHealth = GetComponent<EnemyHealth>();
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        nav.speed = 0.01f;
-        nav.angularSpeed = 300;
+        nav.speed = 0f;
+        nav.angularSpeed = 0f;
         instanceID = GetInstanceID();
     }
 
@@ -29,16 +29,15 @@ public class RepliMovement : MonoBehaviour
         // If the enemy and the player have health left...
         if (enemyHealth.currentHealth > 0 && playerHealth.lives > 0)
         {
-            transform.Translate(0, 0, -.5f * Time.deltaTime);
-            offset -= .5f * Time.deltaTime;
-
-            // ... set the destination of the nav mesh agent to the player.
+            transform.Translate(0, 0, -1f * Time.deltaTime);
+            
+            /*
             Vector3 dest = player.position;
             dest.x += 4 * Mathf.Cos(2 * Time.time + instanceID);
             dest.z += 4 * Mathf.Cos(2 * Time.time + instanceID);
-            nav.SetDestination(dest);
             GetComponent<Transform>().Rotate(dest);
 
+    */
             if (GetComponent<Transform>().position.z < -30)
                 Destroy(gameObject);
         }
