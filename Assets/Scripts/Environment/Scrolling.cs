4b3e58e79ac0c57272edfc5c119892019c20aaba
immefrank <frankzhang93@gmail.com>
2018-01-28 15:33:49 -0800

    Loot material 2
    rt.

--- Assets/Scripts/Environment/Scrolling.cs
+++ Assets/Scripts/Environment/Scrolling.cs
@@ -7,30 +7,41 @@ public class Scrolling : MonoBehaviour
     public float move = -.5f;
     public bool upsideDown = false;
     public bool rotating = false;
-	
-	// Update is called once per frame
-	void Update ()
+
+    GameObject player;                          // Reference to the player GameObject.
+    PlayerHealth playerHealth;
+
+    public void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         if (!upsideDown)
         {
             transform.Translate(0, 0, move * Time.deltaTime);
         }
-        else if (upsideDown)
-        {
-            transform.Translate(0, 0, -move * Time.deltaTime);
-        }
+        //else if (upsideDown)
+        //{
+        //    transform.Translate(0, 0, -move * Time.deltaTime);
+        //}
 
         if (transform.position.z <= -65)
         {
-            transform.position = new Vector3(-2, transform.position.y, 65);
-            transform.rotation = new Quaternion(transform.rotation.x, -180, transform.rotation.z, 0);
-            upsideDown = true;
+
+            //transform.position = new Vector3(-2, transform.position.y, 65);
+            //transform.rotation = new Quaternion(transform.rotation.x, -180, transform.rotation.z, 0);
+            //upsideDown = true;
         }
-        if (transform.position.z <= -25 && upsideDown == true)
-        {
-            transform.position = new Vector3(2, transform.position.y, 25);
-            transform.rotation = new Quaternion(transform.rotation.x, 0, transform.rotation.z, 0);
-            upsideDown = false;
-        }
+        //if (transform.position.z <= -25 && upsideDown == true)
+        //{
+        //    transform.position = new Vector3(2, transform.position.y, 25);
+        //    transform.rotation = new Quaternion(transform.rotation.x, 0, transform.rotation.z, 0);
+        //    upsideDown = false;
+        //}
     }
 }
